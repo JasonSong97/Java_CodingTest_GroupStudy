4b25bdaf1fc882fa3407865236cc5d303ffeabb6
kyuwonhwang <kyuwon.hwang20@gmail.com>
2023-02-17 14:19:48 +0900

    [황규원]2월3주차코딩테스트

--- kyuwonhwang/Programmers/Programmers1.java
+++ kyuwonhwang/Programmers/Programmers1.java
@@ -7,7 +7,7 @@ class Programmers1 {
         System.out.println(solution(5, new int[] { 2, 4 }, new int[] { 1, 3, 5 }));
         System.out.println(solution(5, new int[] { 2, 4 }, new int[] { 3 }));
         System.out.println(solution(3, new int[] { 3 }, new int[] { 1 }));
-        System.out.println(solution(3, new int[] { 1, 2 }, new int[] { 2,3 }));
+        System.out.println(solution(3, new int[] { 2, 1 }, new int[] { 3,2 }));
     }
 
     public static int solution(int n, int[] lost, int[] reserve){
